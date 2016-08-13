1c14ea942930870225a6e09397d4ce452922204d
= <=>
2016-08-13 15:15:57 +0900

    update: change to loop

--- hello.go
+++ hello.go
@@ -3,9 +3,10 @@ package main
 import  "fmt"
 
 // main内でfor実行
+// セミコロン省略可(個人的にはコード規約にセミコロン無しを指定したい)
 func main() {
   sum := 1
-  for ; sum < 1000; {
+  for sum < 1000 {
     sum += sum
   }
   fmt.Println(sum)
