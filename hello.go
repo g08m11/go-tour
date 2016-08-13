e9e214a49d74aad954bdcca8d9f48bc089bd0507
= <=>
2016-08-13 16:59:19 +0900

    update:change to logic. use to time

--- hello.go
+++ hello.go
@@ -2,18 +2,23 @@ package main
 
 import  (
   "fmt"
-  "runtime"
+  "time"
 )
 
+// 他言語同様、switch caseは上からの処理
 func main() {
-  fmt.Print("Go runs on ")
-  switch os := runtime.GOOS; os {
-  case "darwin" :
-    fmt.Println("OS X.")
-  case "linux" :
-    fmt.Println("Linux")
-  default:
-      fmt.Printf("%s.", os)
+  fmt.Print("When's Saturday? ")
+  today := time.Now().Weekday()
+
+  switch time.Saturday {
+  case today + 0 :
+    fmt.Println("Today . ")
+  case today + 1 :
+    fmt.Println("Tommorow. ")
+  case today + 2 :
+    fmt.Println("In tow days.")
+  default :
+    fmt.Println("Too far away.")
   }
 
 }
