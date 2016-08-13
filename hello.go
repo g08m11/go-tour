db6565773706ee3918b32ad669859fdec0f2ddfd
= <=>
2016-08-13 17:33:40 +0900

    update: change logic. use to defer

--- hello.go
+++ hello.go
@@ -3,8 +3,11 @@ package main
 import  "fmt"
 
 // deferは実行処理を遅らせる
+// この場合、fmtの処理はdeferによりLIFOのロジックとなってしまう。
 func main() {
-  defer fmt.Println("world")
-
-  fmt.Println("hello")
+  defer fmt.Println("counting")
+  for i := 0; i < 10; i++ {
+    defer fmt.Println(i)
+  }
+  fmt.Println("done")
 }
