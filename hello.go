7334bb06afeea7f78f62e2b0de66c7b2b24124c4
= <=>
2016-08-13 16:47:02 +0900

    update: change to logic. use to swithc

--- hello.go
+++ hello.go
@@ -2,24 +2,18 @@ package main
 
 import  (
   "fmt"
-  "math"
+  "runtime"
 )
 
-// セミコロンイケる場合といけない場合の区別しづらい。
-// ある条件であれば式を代入するという時はチームで話あう必要がある。
-func pow(x, n, lim float64) float64 {
-  if v := math.Pow(x, n); v < lim {
-    return v
-  } else {
-    fmt.Printf("%g >= %g\n", v, lim)
-  }
-  return lim
-}
-
-// 後述がないのにカンマを入れないといけないのも分かりづらい
 func main() {
-  fmt.Println(
-    pow(3, 2, 10),
-    pow(3, 3, 20),
-  )
+  fmt.Print("Go runs on ")
+  switch os := runtime.GOOS; os {
+  case "darwin" :
+    fmt.Println("OS X.")
+  case "linux" :
+    fmt.Println("Linux")
+  default:
+      fmt.Printf("%s.", os)
+  }
+
 }
