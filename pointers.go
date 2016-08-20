a9e4e006fc7d8ed00e632aea77cc43cd4a1ecdc7
= <=>
2016-08-20 17:24:52 +0900

    update: change to main function. use to nil logic.

--- pointers.go
+++ pointers.go
@@ -5,18 +5,9 @@ import "fmt"
 
 func main() {
 
-  s := [] int { 2, 3, 5, 7, 11, 13 }
-  fmt.Println(s)
-
-  // 1から4までを取得するそのため、取得個数は3
-  s = s[1:4]
-  fmt.Println(s)
-
-  // 0から2個の要素を前から取得する。余ったものは切り捨て
-  s = s[:2]
-  fmt.Println(s)
-
-  // 0から1個の要素を前から取得する。余ったものは切り捨て
-  s = s[1:]
-  fmt.Println(s)
+  var s [] int
+  fmt.Println(s, len(s), cap(s))
+  if s == nil {
+    fmt.Println("Hey! you fuck off ! no value ! nil!")
+  }
 }
