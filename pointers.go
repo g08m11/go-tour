9897daaddca596294bf1b80db3f26e54cbc6df27
= <=>
2016-08-20 16:29:05 +0900

    update: change logic. use to array

--- pointers.go
+++ pointers.go
@@ -3,20 +3,17 @@ package main
 
 import "fmt"
 
-// カンマ区切り同一型で定義するの楽
-type Vertex struct {
-  X, Y int
-}
-
-// goは変数名の大文字、小文字の制御厳しい
-var  (
-  v1 = Vertex{1, 2}
-  v2 = Vertex{X: 1}
-  v3 = Vertex{}
-  p = &Vertex{1, 2}
-)
 
 
 func main() {
-  fmt.Println(v1, p, v2, v3)
+  // array宣言もRubyと似ててイメージつきやすい
+  // 配列のみ指定すると配列型で値を返す
+  // := も使えるため型と値設定を同時に可能
+  var a[2]string
+  a[0] = "Hello"
+  a[1] = "World"
+  fmt.Println(a[0], a[1])
+  fmt.Println(a)
+  primes := [6]int{2, 3, 5, 7, 11, 13}
+  fmt.Println(primes)
 }
