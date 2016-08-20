71382d5e7cf95880218afeed5c2812b54c0acb65
= <=>
2016-08-20 16:32:45 +0900

    update: change main function. use array slice.

--- pointers.go
+++ pointers.go
@@ -3,17 +3,11 @@ package main
 
 import "fmt"
 
-
-
 func main() {
   // array宣言もRubyと似ててイメージつきやすい
-  // 配列のみ指定すると配列型で値を返す
-  // := も使えるため型と値設定を同時に可能
-  var a[2]string
-  a[0] = "Hello"
-  a[1] = "World"
-  fmt.Println(a[0], a[1])
-  fmt.Println(a)
-  primes := [6]int{2, 3, 5, 7, 11, 13}
-  fmt.Println(primes)
+  // 範囲指定の場合はRubyの「..」ではなく「:」なのがpoint
+  primes := [6] int {2, 3, 5, 7, 11, 13}
+
+  var s [] int = primes[1:4]
+  fmt.Println(s)
 }
