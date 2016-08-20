34752cf8d87707b73a271fe40ff9759cef8ebbb7
= <=>
2016-08-20 16:57:27 +0900

    update: change main function. use to array & slice

--- pointers.go
+++ pointers.go
@@ -5,26 +5,18 @@ import "fmt"
 
 func main() {
 
-  q := [] int { 2, 3, 5, 7, 11, 13 }
-  fmt.Println(q)
-
-  r := []bool {true, false, true, true, false, true}
-  fmt.Println(r)
-
-  // ハッシュみたいにkeyとvalueを分けて使うことも出来る?
-  s := [] struct {
-    i int
-    b bool
-  }{
-    {2, true},
-    {3, false},
-    {5, true},
-    {7, true},
-    {11, false},
-    {13, true},
-  }
-
+  s := [] int { 2, 3, 5, 7, 11, 13 }
   fmt.Println(s)
 
+  // 1から4までを取得するそのため、取得個数は3
+  s = s[1:4]
+  fmt.Println(s)
 
+  // 0から2個の要素を前から取得する。余ったものは切り捨て
+  s = s[:2]
+  fmt.Println(s)
+
+  // 0から1個の要素を前から取得する。余ったものは切り捨て
+  s = s[1:]
+  fmt.Println(s)
 }
