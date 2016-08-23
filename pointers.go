f0c88f6ff8e7328a8c2084921bc6fb5df4e87986
Masaru Gushiken <gushiken@zigexn.co.jp>
2016-08-23 17:05:28 +0900

    update: update main function. use to range

--- pointers.go
+++ pointers.go
@@ -3,21 +3,10 @@ package main
 
 import "fmt"
 
-// ruby同様にarrayへの追加をappendで行うので理解しやすい
+var pow = []int{1, 2, 4, 6, 8, 16, 32, 64, 128}
+
 func main() {
-  var s []int
-  printSlice(s)
-
-  s = append(s, 0)
-  printSlice(s)
-
-  s = append(s, 1)
-  printSlice(s)
-
-  s = append(s, 2, 3, 4)
-  printSlice(s)
-}
-
-func printSlice(s []int ) {
-  fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+  for i, v := range pow {
+    fmt.Printf("2**%d = %d\n", i, v)
+  }
 }
