d3dd8e90e60bf73f57f794b359fe28d3dd520182
Masaru Gushiken <gushiken@zigexn.co.jp>
2016-08-23 16:56:08 +0900

    update: update main function.

--- pointers.go
+++ pointers.go
@@ -1,24 +1,27 @@
 // goのmoretypesの学びの内容は全てこのファイルで行う
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "strings"
+)
 
 func main() {
 
-  a := make([]int, 5)
-  printSlice("a", a)
+  board := [][]string {
+    []string{"_", "_", "_"},
+    []string{"_", "_", "_"},
+    []string{"_", "_", "_"},
+  }
 
-  b := make([]int, 0, 5)
-  printSlice("b", b)
+  board[0][0] = "X"
+  board[2][2] = "o"
+  board[1][2] = "X"
+  board[1][0] = "o"
+  board[0][2] = "x"
 
-  c := b[:2]
-  printSlice("c", c)
+  for i := 0; i < len(board); i++ {
+    fmt.Printf("%s\n", strings.Join(board[i], " "))
+  }
 
-  d := c[2:5]
-  printSlice("d", d)
-
-}
-
-func printSlice(s string, x []int) {
-  fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
 }
